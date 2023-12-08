4a6b79b01a3f99788ec89536376a2c67bf54edd7
Enzo Laurence <enzo.laurence9741@gmail.com>
2023-12-08 16:19:08 +0100

    Commentaire

--- Hadock/tintin.py
+++ Hadock/tintin.py
@@ -45,39 +45,59 @@ def send(data):
 
 	s.send(data)
 
-#
+#Fonction permet de calculer un score pour savoir si il s'agit d'un mot
+#Il utilise la frequence a laquelle apparait une lettre dans un mot
 def score_english(plaintext):
-	english_letter_frequency = {'e': 12.70, 't': 9.06, 'a': 8.17, 'o': 7.51, 'i': 6.97, 'n': 6.75, 
+	#Tableau contenant la lettre ainsi que ça fréquence d'apparition
+	#Le tableau contient aussi des character speciaux avec un malus
+	letter_frequency = {'e': 12.70, 't': 9.06, 'a': 8.17, 'o': 7.51, 'i': 6.97, 'n': 6.75, 
 								's': 6.33, 'h': 6.09,'r': 5.99, 'd': 4.25, 'l': 4.03, 'c': 2.78, 
 								'u': 2.76, 'm': 2.41, 'w': 2.36, 'f': 2.23, 'g': 2.02, 'y': 1.97, 
 								'p': 1.93, 'b': 1.49, 'v': 0.98, 'k': 0.77, 'j': 0.15, 'x': 0.15,
 								'q': 0.10, 'z': 0.07, '~': -100, '}': - 100, '{': -100, '|': -100, 
 								'@': -100, '^': -100, '=': -100, '`': -100, ']': -100, '\\': -100}  
     
+	#Passage en minuscule du mot
 	plaintext_lower = plaintext.lower()
-	score = sum(english_letter_frequency.get(char, 0) for char in plaintext_lower)
 
+	#Calcul du score
+	score = sum(letter_frequency.get(char, 0) for char in plaintext_lower)
+
+	#Retour du score
 	return score
 
-#Decode
+#Decode une chaine avec un octect Xoré
 def decode(string):
+	#Prévension des exception en cas d'erreur lors du decodage
 	try:
 		string_decode = bytes.fromhex(string).decode()
 	except UnicodeDecodeError as e:
 		#print(f"Error decoding bytes: {e}")
 		return
 	else: 
+		#Parcours des octect de 90 a 150
 		for i in range(90, 150):
-			test2 = ''
+			value = ''
+			#Parcours de chaque caractere a déchiffrer
 			for caractere in string_decode:
+				#Xor du caractere
 				test = ord(caractere) ^ i
 				if test > 255:
 					test = 0 + test-255
-				test2 += chr(test)
-				score = score_english(test2)
+				#Ajout de la lettre trouvé
+				value += chr(test)
+				#Récupération du scord
+				score = score_english(value)
+
+				#Filtre affin de vérifier le score du mot
 				if score > 70:
-					print(f"{test2} | {score_english(test2)} | {test} | {i}")
-					send(test2.encode())
+					#Affichage de la valeur trouvé
+					print(f"{value} | {score_english(value)} | {test} | {i}")
+
+					#Envoie du code trouvé
+					send(value.encode())
+
+					#Affichage du flag reçu
 					print(receive())
 
 
